d771f29d6ef15712e8d277cf30b8b63c3a3fa773
mgentile11 <michael.l.gentile@uconn.edu>
2017-02-07 12:20:17 -0500

    Add files via upload

---
+++ js/storage.js
@@ -0,0 +1,20 @@
+var Pantry = [];
+
+var Fridge = [];
+
+const foodItem = {
+	name: 'sample-item',
+	amount: 0, // Amount of servings
+	caloriesPerServing:0,
+	purchased: 50000, // Date in miliseconds
+	expire: 50000 // Date in miliseconds
+}
+
+var apples = {
+	name: 'apples',
+	amount:10,
+	caloriesPerServing:100,
+	purchased: 40000000000,
+	expire: 20000000000
+}
+
